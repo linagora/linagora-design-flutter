ad2098d95dfe624db985e0547cd0866684dbee4f
sherlock <quangkhai28999@gmail.com>
2023-04-17 11:21:03 +0700

    TF-6: feature/add example demos for circleAvatar component

---
+++ example/README.md
@@ -0,0 +1,9 @@
+# Linagora_design_Flutter examples
+Demonstrates how to use the [linagora-design-flutter](https://github.com/linagora/linagora-design-flutter)
+
+## CircleAvatar Example
+CircleAvatar ios demo
+<img src="./assets/images/circle_avatar_ios.png" width=400>
+
+CircleAvatar web demo
+<img src="./assets/images/circle_avatar_web.png">
---
+++ example/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:example/demos/circle_avatar_example.dart';
+import 'package:example/demos/circle_avatar_load_from_memory_example.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Linagora Design Component Examples',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Examples'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          DemoTile(component: CircleAvatarExample(), title: 'Demo CircleAvatar component',),
+          DemoTile(title: 'Demo CircleAvatar component with load image from bytes', component: CircleAvatarLoadFromMemoryExample()),
+        ],
+      ),
+    );
+  }
+
+}
+
+class DemoTile extends StatelessWidget {
+  final String title;
+
+  final Widget component;
+
+  const DemoTile({
+    super.key, 
+    required this.title, 
+    required this.component
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => component),
+        );
+      },
+      style: ButtonStyle(
+        minimumSize: MaterialStateProperty.all(Size(100, 50))
+      ),
+      child: Text('$title'),
+    );
+  }
+}
