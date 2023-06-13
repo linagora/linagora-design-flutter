34643f6e4fe9a55a7712fd74834fed25a8c1810a
sherlock <quangkhai28999@gmail.com>
2023-06-13 15:16:50 +0700

    TW-130: add assetCounter for asset

---
+++ lib/images_picker/asset_counter.dart
@@ -0,0 +1,86 @@
+
+import 'package:flutter/material.dart';
+
+typedef IsSelectedNotifier =  ValueNotifier<bool>;
+
+typedef SelectedIndexNotifier = ValueNotifier<int>;
+
+class AssetCounter extends ChangeNotifier {
+  
+  final Map<int, IsSelectedNotifier> _isSelectedMap = {};
+
+  final Map<int, SelectedIndexNotifier> _selectedIndexMap = {};
+
+  int _totalSelected = 0;
+
+  void initializeIfNeeded({required int index}) {
+    _initializeIsSelectedIfNeeded(index: index);
+    _initializeSelectedIndexIfNeeded(index: index);
+  }
+
+  _initializeSelectedIndexIfNeeded({required int index}) {
+    _selectedIndexMap.putIfAbsent(index, () => ValueNotifier(0));
+  }
+
+  void _initializeIsSelectedIfNeeded({required int index}) {
+    if (!_isSelectedMap.keys.contains(index)) {
+      _isSelectedMap[index] = ValueNotifier<bool>(false);
+      _registerListenerInSelectionAt(index);
+    }
+  }
+
+  void toggleAssetSelection(int index) {
+    if (_isSelectedMap[index] != null) {
+      _isSelectedMap[index]!.value = !_isSelectedMap[index]!.value;
+    }
+  }
+
+  void _registerListenerInSelectionAt(int index) {
+    _isSelectedMap[index]!.addListener(() {
+      if (_isSelectedMap[index]!.value) {
+        _totalSelected++;
+        _selectedIndexMap[index]!.value = _totalSelected;
+      } else {
+        _totalSelected--;
+        _updateLargerSelectedIndexes(index);
+      }
+      notifyListeners();
+    });
+  }
+  
+  void _updateLargerSelectedIndexes(int index) {
+    int tmp = _selectedIndexMap[index]!.value;
+    for(final key in _selectedIndexMap.keys) {
+      if (key == index) {
+        _selectedIndexMap[key]!.value = 0;
+      }
+      if (_selectedIndexMap[key]!.value > tmp) {
+          _selectedIndexMap[key]!.value--;
+      }
+    }
+  }
+
+  void registerListenerAtSelectedIndex(int index, VoidCallback listener) {
+    if (_selectedIndexMap[index] != null) {
+      _selectedIndexMap[index]!.addListener(listener);
+    }
+  }
+
+  int getSelectedIndexAt(int index) {
+    return _selectedIndexMap[index]?.value ?? 0;
+  }
+
+  int get currentTotalSelectedIndex {
+    return _totalSelected;
+  }
+
+  bool isSelected(int index) {
+    return _isSelectedMap[index]?.value ?? false;
+  }
+
+  List<int> get selectedIndexes {
+    return _selectedIndexMap.keys.where(
+      (key) => _isSelectedMap[key]?.value ?? false
+    ).toList();
+  }
+}
---
+++ test/image_picker/asset_counter_test.dart
@@ -0,0 +1,138 @@
+@TestOn('vm')
+import 'package:flutter_test/flutter_test.dart';
+import 'package:linagora_design_flutter/images_picker/asset_counter.dart';
+
+void main() {
+  final AssetCounter assetCounter = AssetCounter();
+
+  setUp(() {
+    assetCounter = AssetCounter();
+    // initialize asset counter for images from 0 to 5
+    assetCounter.initializeIfNeeded(index: 0);
+    assetCounter.initializeIfNeeded(index: 1);
+    assetCounter.initializeIfNeeded(index: 2);
+    assetCounter.initializeIfNeeded(index: 3);
+    assetCounter.initializeIfNeeded(index: 4);
+    assetCounter.initializeIfNeeded(index: 5);
+  });
+
+  group("AssetCounter class test", () {
+    test("""GIVEN user did not select any item WHEN user select item number 2 
+      THEN item number 2 should be selected
+      AND its correspond selected index: 1""", () {
+      assetCounter.toggleAssetSelection(2);
+
+      //EXPECTED
+      expect(assetCounter.currentTotalSelectedIndex, 1);
+      expect(assetCounter.isSelected(2), true);
+      expect(assetCounter.getSelectedIndexAt(2), 1);
+    });
+
+    test("""GIVEN user did not select any item 
+      WHEN user select item number 1, item number 2, item number 3 
+      THEN item number 1, 2, 3 should be selected
+      AND its correspond selected index: 1, 2, 3""", () {
+      
+      assetCounter.toggleAssetSelection(1);
+      assetCounter.toggleAssetSelection(2);
+      assetCounter.toggleAssetSelection(3);
+
+      //EXPECTED
+      expect(assetCounter.currentTotalSelectedIndex, 3);
+      expect(assetCounter.isSelected(1), true);
+      expect(assetCounter.isSelected(2), true);
+      expect(assetCounter.isSelected(3), true);
+      expect(assetCounter.isSelected(4), false);
+      expect(assetCounter.getSelectedIndexAt(1), 1);
+      expect(assetCounter.getSelectedIndexAt(2), 2);
+      expect(assetCounter.getSelectedIndexAt(3), 3);
+      expect(assetCounter.getSelectedIndexAt(4), 0);
+    });
+
+    test("""GIVEN user did not select any item 
+      WHEN user select item number 1, item number 2, item number 3 and unselect item number 1 
+      THEN item number 2, 3 should be selected
+      AND its correspond selected index: 1, 2""", () {
+      assetCounter.toggleAssetSelection(1);
+      assetCounter.toggleAssetSelection(2);
+      assetCounter.toggleAssetSelection(3);
+      assetCounter.toggleAssetSelection(1);
+
+      //EXPECTED
+      expect(assetCounter.currentTotalSelectedIndex, 2);
+      expect(assetCounter.isSelected(1), false);
+      expect(assetCounter.isSelected(2), true);
+      expect(assetCounter.isSelected(3), true);
+      expect(assetCounter.isSelected(4), false);
+      expect(assetCounter.getSelectedIndexAt(1), 0);
+      expect(assetCounter.getSelectedIndexAt(2), 1);
+      expect(assetCounter.getSelectedIndexAt(3), 2);
+      expect(assetCounter.getSelectedIndexAt(4), 0);
+    });
+
+    test("""GIVEN user did not select any item 
+      WHEN user select item number 5, 4, 2, 1 then toggle item number 5 two times more
+      THEN item number 5, 4, 2, 1 should be selected
+      AND its correspond selected index: 4, 1, 2, 3""", () {
+      assetCounter.toggleAssetSelection(5);
+      assetCounter.toggleAssetSelection(4);
+      assetCounter.toggleAssetSelection(2);
+      assetCounter.toggleAssetSelection(1);
+      assetCounter.toggleAssetSelection(5);
+      assetCounter.toggleAssetSelection(5);
+
+
+      //EXPECTED
+      expect(assetCounter.currentTotalSelectedIndex, 4);
+      expect(assetCounter.isSelected(1), true);
+      expect(assetCounter.isSelected(2), true);
+      expect(assetCounter.isSelected(3), false);
+      expect(assetCounter.isSelected(4), true);
+      expect(assetCounter.isSelected(5), true);
+      expect(assetCounter.getSelectedIndexAt(1), 3);
+      expect(assetCounter.getSelectedIndexAt(2), 2);
+      expect(assetCounter.getSelectedIndexAt(3), 0);
+      expect(assetCounter.getSelectedIndexAt(4), 1);
+      expect(assetCounter.getSelectedIndexAt(5), 4);
+    });
+
+    test("""GIVEN user did not select any item 
+      WHEN user select item number 5, 4, 2, 1 then user scroll down to load more item,
+      and select item 10, 9 then unselect item number 1
+      THEN item number 1,2,4,9,10 should be selected
+      AND its correspond selected index: 3, 2, 1, 5, 4 """, () {
+      assetCounter.toggleAssetSelection(5);
+      assetCounter.toggleAssetSelection(4);
+      assetCounter.toggleAssetSelection(2);
+      assetCounter.toggleAssetSelection(1);
+
+      // load next 5 images
+      assetCounter.initializeIfNeeded(index:6);
+      assetCounter.initializeIfNeeded(index:7);
+      assetCounter.initializeIfNeeded(index:8);
+      assetCounter.initializeIfNeeded(index:9);
+      assetCounter.initializeIfNeeded(index:10);
+
+      assetCounter.toggleAssetSelection(10);
+      assetCounter.toggleAssetSelection(9);
+      assetCounter.toggleAssetSelection(5);
+
+      //EXPECTED
+      expect(assetCounter.currentTotalSelectedIndex, 5);
+      expect(assetCounter.isSelected(1), true);
+      expect(assetCounter.isSelected(2), true);
+      expect(assetCounter.isSelected(3), false);
+      expect(assetCounter.isSelected(4), true);
+      expect(assetCounter.isSelected(5), false);
+      expect(assetCounter.isSelected(9), true);
+      expect(assetCounter.isSelected(10), true);
+      expect(assetCounter.getSelectedIndexAt(1), 3);
+      expect(assetCounter.getSelectedIndexAt(2), 2);
+      expect(assetCounter.getSelectedIndexAt(3), 0);
+      expect(assetCounter.getSelectedIndexAt(4), 1);
+      expect(assetCounter.getSelectedIndexAt(5), 0);
+      expect(assetCounter.getSelectedIndexAt(9), 5);
+      expect(assetCounter.getSelectedIndexAt(10), 4);
+    });
+  });
+}
