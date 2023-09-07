38ea782fa175480f7939e9b6cffb6d504d81a15b
HuyNguyen <quanghnguyen@linagora.com>
2023-09-07 10:46:04 +0700

    Publish `LinagoraTextStyle`

--- lib/linagora_design_flutter.dart
+++ lib/linagora_design_flutter.dart
@@ -6,3 +6,4 @@ export 'package:linagora_design_flutter/colors/linagora_key_colors.dart';
 export 'package:linagora_design_flutter/colors/linagora_state_layer.dart';
 export 'package:linagora_design_flutter/avatar/avatar.dart';
 export 'package:linagora_design_flutter/images_picker/images_picker.dart';
+export 'package:linagora_design_flutter/style/linagora_text_style.dart';
