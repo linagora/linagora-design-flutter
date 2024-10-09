e4f2976f20508dc16c677d86db150d2efa86968f
HuyNguyen <quanghnguyen@linagora.com>
2024-10-09 14:32:39 +0700

    fixup! fixup! Added listItem,TwakeInkwell and demo

--- lib/style/list_item_style.dart
+++ lib/style/list_item_style.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:linagora_design_flutter/colors/linagora_ref_colors.dart';
+import 'package:linagora_design_flutter/linagora_design_flutter.dart';
 
 class ListItemStyle {
   static const double height = 80.0;
@@ -7,4 +7,20 @@ class ListItemStyle {
       const BorderRadius.all(Radius.circular(4));
   static final Color backgroundColor =
       LinagoraRefColors.material().primary[100]!;
+
+  static TextStyle titleTextStyle({
+    required String fontFamily,
+  }) =>
+      LinagoraTextStyle.material().bodyMedium2.copyWith(
+            color: LinagoraSysColors.material().onSurface,
+            fontFamily: fontFamily,
+          );
+
+  static TextStyle subtitleTextStyle({
+    required String fontFamily,
+  }) =>
+      LinagoraTextStyle.material().bodyMedium.copyWith(
+            color: LinagoraSysColors.material().onSurface,
+            fontFamily: fontFamily,
+          );
 }
