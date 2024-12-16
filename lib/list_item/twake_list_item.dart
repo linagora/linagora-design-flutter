a0d120bce05c5d7885f4728cbc41017c7f19838a
khaled.njim <knjim@linagora.com>
2024-12-16 13:41:02 +0100

    Added padding and margin  attributes to TwakeListItem

--- lib/list_item/twake_list_item.dart
+++ lib/list_item/twake_list_item.dart
@@ -4,16 +4,22 @@ import 'package:linagora_design_flutter/style/linagora_divider_style.dart';
 class TwakeListItem extends StatelessWidget {
   final Widget child;
   final double? height;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
 
   const TwakeListItem({
     Key? key,
     required this.child,
     this.height,
+    this.padding,
+    this.margin,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: margin,
+      padding: padding,
       height: height,
       decoration: BoxDecoration(
         border: LinagoraDividerStyle.material().borderDecoration,
