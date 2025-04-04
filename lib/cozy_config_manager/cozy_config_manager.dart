26b86c80f07d89690cf1be151136f87931b04f5c
DatDang <tddang@linagora.com>
2025-04-04 15:55:34 +0700

    Cozy integration

---
+++ lib/cozy_config_manager/cozy_config_manager.dart
@@ -0,0 +1,2 @@
+export 'cozy_config_manager_stub.dart'
+    if (dart.library.html) 'cozy_config_manager_web.dart';
--- lib/linagora_design_flutter.dart
+++ lib/linagora_design_flutter.dart
@@ -14,3 +14,4 @@ export 'package:linagora_design_flutter/style/list_item_style.dart';
 export 'package:linagora_design_flutter/style/linagora_divider_style.dart';
 export 'package:linagora_design_flutter/style/linagora_hover_style.dart';
 export 'package:linagora_design_flutter/list_item/twake_inkwell.dart';
+export 'package:linagora_design_flutter/cozy_config_manager/cozy_config_manager.dart';
