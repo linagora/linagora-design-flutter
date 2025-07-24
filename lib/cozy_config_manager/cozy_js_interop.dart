6ea2010ea52c74128d7cabf52a494c974c88b06a
DatDang <tddang@linagora.com>
2025-07-24 16:59:15 +0700

    Add support for request and send notification when inside Cozy dashboard

--- lib/cozy_config_manager/cozy_js_interop.dart
+++ lib/cozy_config_manager/cozy_js_interop.dart
@@ -11,3 +11,19 @@ external bool? setupBridgeJs(String targetOrigin);
 
 @JS('window._cozyBridge.startHistorySyncing')
 external void startHistorySyncingJs();
+
+@JS('window._cozyBridge.requestNotificationPermission')
+external JSPromise<JSString?> requestNotificationPermissionJs();
+
+@JS('window._cozyBridge.sendNotification')
+external void sendNotificationJs(CozyNotificationData data);
+
+extension type CozyNotificationData(JSObject _) implements JSObject {
+  external factory CozyNotificationData.create({
+    JSString title,
+    JSString body,
+  });
+
+  external JSString get title;
+  external JSString get body;
+}
