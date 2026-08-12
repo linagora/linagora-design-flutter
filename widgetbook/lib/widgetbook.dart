046b778515e879032324f903df3266d78259fb38
HuyNguyen <quanghnguyen@linagora.com>
2026-08-12 15:33:53 +0700

    Simplify viewport configuration in widgetbook

--- widgetbook/lib/widgetbook.dart
+++ widgetbook/lib/widgetbook.dart
@@ -29,12 +29,7 @@ class WidgetbookApp extends StatelessWidget {
       darkTheme: ThemeData.light(),
       addons: [
         GitHubAddon('widgetbook'),
-        ViewportAddon(
-          [
-            IosViewports.iPhone13,
-            IosViewports.iPad,
-          ],
-        ),
+        ViewportAddon(Viewports.all),
         InspectorAddon(),
         ThemeAddon(
           themes: [
